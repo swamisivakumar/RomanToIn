28041b975441108eaabd2562916a2a3895fc8a39
Swamy Yalamati <swyalamati@teksystems.com>
2023-03-02 19:30:01 +0530

    Add project files.

---
+++ RomanToIn/CharExtend.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanToIn
+{
+    public static class CharExtend
+    {
+        public static int RN(this char c)
+        {
+            switch (c)
+            {
+                case 'I': return 1;
+                case 'V': return 5;
+                case 'X': return 10;
+                case 'L': return 50;
+                case 'C': return 100;
+                case 'D': return 500;
+                case 'M': return 1000;
+            };
+            return 0;
+        }
+
+
+    }
+}
---
+++ RomanToIn/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace RomanToIn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            var x = p.RomanToInteger("MCMXCIV");
+            var x = p.RomanToInteger("LVIII");
+            Console.WriteLine(x);
+            Console.ReadLine();
+        }
+
+        public  string RomanToInteger(string str)
+        {
+
+            int value = 0;
+            char[] chars = str.ToCharArray();
+            var resultValues = chars.Select(x => x.RN()).ToArray<int>();
+            int[] ints = new int[chars.Length];
+            for(int i=0;i< resultValues.Length-1;i++)
+            {
+                if (resultValues[i] >= resultValues[i + 1])
+                {
+                    ints[i]=resultValues[i];
+                }
+                else
+                {
+                    ints[i] = (resultValues[i]*-1);
+                }
+            }
+            ints[chars.Length-1] = resultValues[chars.Length-1];
+            for(int i=0;i<chars.Length;i++)
+            {
+                value += ints[i];
+            }     
+            return value.ToString();        
+            
+        }
+
+     
+
+
+
+    }
+}
